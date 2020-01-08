d5282e8afe397e1f9fdb24ce4394296dc73ff5a6
matbotW <mathewmcdade@gmail.com>
2020-01-08 07:12:10 -0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ExpenseApp());
+
+class ExpenseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Expense App",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final List
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense App"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.lightBlueAccent,
+              child: Text("CHART HERE"),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            color: Colors.redAccent,
+            child: Text("TEXTTEXTTEXT"),
+          ),
+        ],
+      ),
+    );
+  }
+}

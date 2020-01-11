f11a470b153b8937f3e2b65faa391ea9dd3d4520
matbotW <mathewmcdade@gmail.com>
2020-01-10 20:26:55 -0800

    using ListView widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,51 +12,53 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((trxn) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin:
-                EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.green,
-                    width: 2,
+    return Container(
+      height: 400,
+      child: ListView(
+        children: transactions.map((trxn) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.green,
+                      width: 2,
+                    ),
                   ),
-                ),
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  /* String Interpolation */
-                  "\$${trxn.price}",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                      color: Colors.green),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    trxn.title,
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    /* String Interpolation */
+                    "\$${trxn.price}",
                     style: TextStyle(
-                        fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    DateFormat.yMMMd().format(trxn.date),
-                    style: TextStyle(
-                        fontSize: 14,
                         fontWeight: FontWeight.bold,
-                        color: Colors.grey),
+                        fontSize: 24,
+                        color: Colors.green),
                   ),
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      trxn.title,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(trxn.date),
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
